fb3d47fe3fba93caab82b28c0e487c42edb37ef4
Cyber Worm <72599968+bellurm@users.noreply.github.com>
2022-08-04 19:44:52 +0300

    Add files via upload

--- 4wayVariables.go
+++ 4wayVariables.go
@@ -1,17 +1,18 @@
 package main
 
-import "fmt"
-
 func print4way() {
-	var studentName, grade, isPassed = "John Doe", 77, true
-	fmt.Println(studentName)
-	fmt.Println(grade)
-	fmt.Println(isPassed)
+	/*
+		var studentName, grade, isPassed = "John Doe", 77, true
+		fmt.Println(studentName)
+		fmt.Println(grade)
+		fmt.Println(isPassed)
+	*/
 
-	/* studentName, grade, isPassed := "John Doe", 77, true
-	fmt.Println(studentName)
-	fmt.Println(grade)
-	fmt.Println(isPassed)
+	/*
+		studentName, grade, isPassed := "John Doe", 77, true
+		fmt.Println(studentName)
+		fmt.Println(grade)
+		fmt.Println(isPassed)
 	*/
 
 	/*
@@ -35,6 +36,7 @@ func print4way() {
 		fmt.Println(grade)
 		fmt.Println(isPassed)
 	*/
+
 	/*
 		studentName := "John Doe"
 		grade := 77
